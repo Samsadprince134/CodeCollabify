c5905ad401bd6c69cd48da9d2839ddd4913d5fdb
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-08-15 01:50:16 +0530

    Update vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,5 +1,5 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
@@ -9,28 +9,14 @@
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true, // Ensure source maps are generated
-//   },
-// });
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
+    sourcemap: true, // Ensure source maps are generated
   },
-})
+});
+
+
+
 
